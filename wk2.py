bccdfc89cb82a436b640527137fb5f4e32c42f69
tgivens1 <tgivens@mail.walshcollege.edu>
2022-04-06 21:05:06 -0400

    first name lower, last name upper

---
+++ wk2.py
@@ -0,0 +1,15 @@
+# 1. Stores your first name as a variable. Use all lowercase letters when you declare it.
+
+first_name = "Thomas" #Storing first name as variable
+
+print(first_name.lower()) #declaring in lower case letters
+
+# 2. Stores your last name as a variable. Use all uppercase letters when you declare it.
+
+last_name = "Givens"  #storing last name
+
+print(last_name.upper)())  #delcaring in upper case letters 
+
+# 3.	Prints out, "Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions.
+
+sting_var = "Hello"
