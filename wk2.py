46666c267ee6222ea095e27aa50728526b39b532
tgivens1 <tgivens@mail.walshcollege.edu>
2022-04-06 22:57:32 -0400

    new variable, slice, replace

--- wk2.py
+++ wk2.py
@@ -12,10 +12,27 @@ print(last_name.upper())  #delcaring in upper case letters
 
 # 3.	Prints out, "Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions.
 
-string_var = "Hello" #Storing Hello as variable 
-
-print(string_var + "," " " + first_name.upper() + " " + last_name.lower())  # Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions.
+print("Hello," +  " " + first_name.upper() + " " + last_name.lower())  # Hello, <first name> <last name>" with the first name converted to uppercase letters and the last name converted to lowercase letters using string functions.
 
 #4.	Prints out two newlines.
 
 print(2* "\n")  
+
+# 5.	Creates a new variable that stores your first and last name together with a space between both parts.
+
+my_name = "Thomas" + " Givens"  #storing
+print(my_name)                  #printing
+
+#6.	Slices your last name from the variable you created in step 5 and prints it out. This must take place on one line. 
+
+print(my_name[7:13])  #slicing last name from variable starts at 7 and ends at 13
+
+# 7.	Replaces your last name in the variable you created in step 5 with "<your last name>, Walsh College Student"; print out the new value of this variable.
+
+print(my_name.replace("Givens", "Givens, Walsh College Student")) #replacing last name 
+
+
+
+
+
+
