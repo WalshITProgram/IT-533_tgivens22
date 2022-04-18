01bd8b7bc95a0bc7cc67d02d5e0a929d15f811e7
tgivens1 <tgivens@mail.walshcollege.edu>
2022-04-17 21:16:44 -0400

    3

--- wk3.py
+++ wk3.py
@@ -1,4 +1,14 @@
+import csv
+import json
+
 sales_data = []   # creating blank list 
 
-#csv_file = open("SalesJan2022.csv")  #opening file, I had to change the name to open it 
+csv_file = open("SalesJan2022.csv")  #opening file to read
+
+with open("SalesJan2022.csv", "r", newline= '') as csv_file: 
+    filtered = (line.replace('\r', '') for line in csv_file)  #creating new iterator, removing \n, cleaning csv 
+    csv_reader = csv.reader(csv_file, delimiter=',')          #passing file through reader setting delimiter 
+
+
+
 
