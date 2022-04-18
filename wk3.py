03604dd7cfda68b321e47cf21db1e63de54f645e
tgivens1 <tgivens@mail.walshcollege.edu>
2022-04-17 21:47:44 -0400

    dictionaires

--- wk3.py
+++ wk3.py
@@ -15,6 +15,14 @@ with open("SalesJan2022.csv", "r", newline= '') as csv_file:  #opening csv, sett
         if(i ==10):
             break
 
-
-
+#creating dictionaries, adding headers 
+first_d = {"Transaction_Date": "1/2/2009 6:17", "Product": "Product 1", "Price": "1200", "Payment_Type": "Mastercard", "Name": "carolina", "City": "Basildon","State": "England", "Country": "United Kingdom"}
+second_d = {"Transaction_Date": "1/2/2009, 4:53", "Product": "Product 1", "Price": "1200", "Payment_Type": "Visa", "Name": "Betina", "City": "Parkville", "State":  "MO", "Country":  "United States"}
+third_d = {"Transaction_Date": "1/2/2009, 13:08", "Product": "Product 1","Price": "1200", "Payment_Type":"Mastercard", "Name": "Federica e Andrea", "City": "Astoria","State": "OR", "Country": "United States"}
+fourth_d = {"Transaction_Date": "1/3/2009, 14:44", "Product": "Product 1", "Price": "1200", "Payment_Type": "Visa", "Name": "Gouya", "City": "Echuca", "State": "Victoria", "Country": "Australia"}
+fifth_d = {"Transaction_Date": "1/4/2009, 12:56", "Product": "Product 2", "Price": "3600", "Payment_Type": "Visa", "Name": "Gerd W", "City": "Cahaba Heights", "State": "AL", "Country": "United States"}
+sixth_d = {"Transaction_Date": "1/4/2009, 12:56", "Product": "Product 2", "Price": "3600", "Payment_Type": "Visa", "Name": "Gerd W", "City": "Cahaba Heights", "State": "AL", "Country": "United States"}
+seventh_d = {"Transaction_Date": "1/4/2009, 20:11", "Product": "Product 1", "Price": "1200", "Payment_Type": "Mastercard", "Name": "Fleur", "City": "Peoria", "State": "IL", "Country": "United States"}
+eight_d = {"Transaction_Date": "1/2/2009, 20:09", "Product": "Product 1", "Price": "1200", "Payment_Type": "Mastercard", "Name": "adam", "City": "Martin", "State": "TN", "Country": "United States"}
+ninth_d = {"Transaction_Date": "1/4/2009, 13:17", "Product": "Product 1", "Price": "1200", "Payment_Type": "Mastercard", "Name": "Renee Elisabeth", "City": "Tel Aviv", "State": "Tel Aviv", "Country": "Israel"}
 
