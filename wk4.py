a8828ed4a883afa03d04ccb6f4cc68dee962faa7
tgivens1 <tgivens@mail.walshcollege.edu>
2022-04-25 23:25:55 -0400

    1

---
+++ wk4.py
@@ -0,0 +1,3 @@
+ltv = float(input("Enter a value for ltv:"))  #using the float data type for the non intergers
+if ltv > int(79.99):                          # if statement, input more than 79.99 mortage insurance is required
+    print("Mortgage Insurance is required")   # response for user
