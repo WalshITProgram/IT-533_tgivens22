1b5885c90359967a4be9ae287daffe75b823bc9e
tgivens1 <tgivens@mail.walshcollege.edu>
2022-06-08 23:05:58 -0400

    valid name

--- draft 910.py
+++ draft 910.py
@@ -42,6 +42,21 @@ class validator():
 # if bad charcters from the bad characters check are found in the enter_email varibale a message will appear letting the user know invalid characters have been found.
 # If no characters are found the function will be returned, if an email is not entered the invalid email format message will appear
 
+        def valid_name():
+            while True:   
+                enter_name = input("Please enter name : ")
+                if re.match("^[a-zA-Z][a-zA-Z '-]", enter_name):
+                    if bad_characters_check(enter_name, bad_characters):
+                        print("Invalid characters found.")
+                    else:
+                        return enter_name
+                else:
+                    print("Invalid name entered")
+
+# valid name function - while true the user will enter the name, the name will have to match the format in the regular expression line. 
+# if bad charcters from the bad characters check are found in the print message will appear letting the user know invalid characters have been found.
+# If no characters are found the function will be returned, if a name is not entered the invalid email format message will appear              
+
 
                 
 
