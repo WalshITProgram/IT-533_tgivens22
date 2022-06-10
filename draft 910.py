4db1126d26bb7dbd720a61e879a658bfd6b72c60
tgivens1 <tgivens@mail.walshcollege.edu>
2022-06-10 19:11:15 -0400

    notes

--- draft 910.py
+++ draft 910.py
@@ -1,16 +1,22 @@
 import re             #importing regular expressions, to use with the name class to ensure input is the letters a-z lower or uppercase
 
 class display_information:
-
-    def __repr__(self):                                                     #repr - used to return a printable version of the object
+    '''#repr - used to return a printable version of the object
+       #will iterate through instances and update the dictionary with the results, using the name of the attributes as the key
+    '''
+    def __repr__(self):                                                     
         item = {}
         for key in self.__dict__:
             item.update({key: getattr(self,key)})
         return str(item)
 
-#  will iterate through instances and update the dictionary with the results, using the name of the attributes as the key
+
 
 class validator():
+    '''#creating class - validator 
+       class will make sure user input is valid, for the name, id, email
+       the items that the student and teacher have in common. Each items has criteria that must be present or absent to be considered accurate
+    '''
     def __init__(self):
         bad_characters = ['!', '"', "'", '#', '$',                    #list of prohibited characters for the email
                                 '%', '^', '&', '*', '(', 
@@ -24,7 +30,7 @@ class validator():
                 if character in bad_characters_used:
                     return True
 
-#creating class - validator
+
 #creating a function to check for bad characters, iterate through each character entered and if bad characters are found the function will be returned as true
 
         def valid_email():
